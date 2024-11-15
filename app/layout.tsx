eff574448f8fc613be08f848149789ee70d5dcf2
Pablo Gracia Gil <pablo.graciag@gmail.com>
2024-11-15 09:18:20 +0100

    Add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
-import { SpeedInsights } from "@vercel/speed-insights/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 import { Layout } from "@/components/Layout";
 
@@ -22,6 +23,7 @@ export default function RootLayout({
           <Layout>{children}</Layout>
         </div>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
